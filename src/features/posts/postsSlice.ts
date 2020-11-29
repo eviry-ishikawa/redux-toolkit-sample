1cc9f4907fbd7b090089c38b2f802c35492a51e2
eviry-ishikawa <ishikawa@eviry.com>
2020-11-29 23:50:57 +0900

    add reaction action to postslice

--- src/features/posts/postsSlice.ts
+++ src/features/posts/postsSlice.ts
@@ -1,11 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 
+export type Reactions = {
+  thumbsUp: number;
+  hooray: number;
+  heart: number;
+  rocket: number;
+  eyes: number;
+};
+
 export type Post = {
   id: number;
   title: string;
   content: string;
   user: number;
+  reactions: Reactions;
 };
 
 export type Posts = Post[];
@@ -16,25 +25,37 @@ const initialState: Posts = [
     title: 'test title1',
     content: 'test content',
     user: 1,
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
   },
   {
     id: 2,
     title: 'test title2',
     content: 'test content',
     user: 1,
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
   },
   {
     id: 3,
     title: 'test title3',
     content: 'test content',
     user: 1,
+    reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 },
   },
 ];
 
 export const postsSlice = createSlice({
   name: 'posts',
   initialState: initialState,
-  reducers: {},
+  reducers: {
+    reactionAdded: (state, action) => {
+      const {
+        postId,
+        reaction,
+      }: { postId: number; reaction: keyof Reactions } = action.payload;
+      const post = state.find((post) => post.id === postId);
+      post?.reactions[reaction];
+    },
+  },
 });
 
 export const selectAllPost = (state: RootState): Posts => state.posts;
