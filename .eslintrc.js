e638331679553a4da85ae54ab78b9f90de556ba8
eviry-ishikawa <ishikawa@eviry.com>
2020-11-29 14:55:29 +0900

    add lint

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+}
